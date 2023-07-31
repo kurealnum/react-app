85e3f4d53a09136032bbcd346b7c5e190a57231b
kurealnum <cattledoger@gmail.com>
2023-07-30 21:27:03 -0400

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,24 @@
 import { Fragment } from "react";
 
 function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  if (items.length === 0) {
+    return (
+      <>
+        <h1>List</h1>
+        <p>No items found!</p>
+      </>
+    );
+  }
+
   return (
     <>
       <ul className="list-group">
-        <li className="list-item">item 1</li>
-        <li className="list-item">item 2</li>
-        <li className="list-item">item 3</li>
-        <li className="list-item">item 4</li>
-        <li className="list-item">itme 5</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
