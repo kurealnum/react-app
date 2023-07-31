5c8e2266656519e4270181962da323eb0eabeaac
kurealnum <cattledoger@gmail.com>
2023-07-30 21:21:56 -0400

    added fragments

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,17 @@
+import { Fragment } from "react";
+
 function ListGroup() {
-  return <h1>List Group</h1>;
+  return (
+    <>
+      <ul className="list-group">
+        <li className="list-item">item 1</li>
+        <li className="list-item">item 2</li>
+        <li className="list-item">item 3</li>
+        <li className="list-item">item 4</li>
+        <li className="list-item">itme 5</li>
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
