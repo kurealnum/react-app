2f01e43a8af013ab022b0893db1e68295f1cd148
kurealnum <cattledoger@gmail.com>
2023-07-30 20:48:37 -0400

    formatting + new file

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,10 +1,9 @@
-function Message(){
-    const name = "Oscar";
-    if (name) {
-        return <h1>Hello {name}</h1>;
-    }
-    return <h1>Hello World</h1>
-    
+function Message() {
+  const name = "Oscar";
+  if (name) {
+    return <h1>Hello {name}</h1>;
+  }
+  return <h1>Hello World</h1>;
 }
 
 export default Message;
